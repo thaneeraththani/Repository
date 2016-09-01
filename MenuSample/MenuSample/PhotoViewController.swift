a64c9a2a4c4906ea0bdbeccb3985cdc40248af48
Thaneerath <thaneerath.th@citrusinformatics.com>
2016-09-01 13:11:20 +0530

    Next commit

---
+++ MenuSample/MenuSample/PhotoViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoViewController.swift
+//  MenuSample
+//
+//  Created by Thaneerath on 31/08/16.
+//  Copyright © 2016 citrusinformatics. All rights reserved.
+//
+
+import Cocoa
+
+class PhotoViewController: UIViewController {
+
+}
---
+++ MenuSample/MenuSample/TabViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TabViewController.swift
+//  MenuSample
+//
+//  Created by Thaneerath on 31/08/16.
+//  Copyright © 2016 citrusinformatics. All rights reserved.
+//
+
+import Cocoa
+
+class TabViewController: UIViewController {
+
+}
