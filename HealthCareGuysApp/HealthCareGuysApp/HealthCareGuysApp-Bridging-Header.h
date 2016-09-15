bd70827de76bb5363f1a12863fe0fc53b66ba5cc
Thaneerath T H <thaneerath.th@citrusinformatics.com>
2016-09-15 16:07:18 +0530

    Parser method and data Insertion method added

---
+++ HealthCareGuysApp/HealthCareGuysApp/HealthCareGuysApp-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  HealthCareGuysApp-Bridging-Header.h
+//  HealthCareGuysApp
+//
+//  Created by Thaneerath on 15/09/16.
+//  Copyright © 2016 citrusinformatics. All rights reserved.
+//
+#import "SWRevealViewController.h"
